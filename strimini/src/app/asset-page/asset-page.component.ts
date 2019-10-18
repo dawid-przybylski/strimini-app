2e0b3f57d176c39ab1e6c2d4e88c6bb8bbf90536
kozaqui <kozaqui@gmail.com>
2019-10-19 00:41:14 +0200

    Change logos to png, add 'Mentioned' count

--- strimini/src/app/asset-page/asset-page.component.ts
+++ strimini/src/app/asset-page/asset-page.component.ts
@@ -13,13 +13,13 @@ export class AssetPageComponent implements OnInit {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'BBC', cols: 1, rows: 1, img: 'assets/img/bbc-logo.png' },
-          { title: 'Al Jazeera', cols: 1, rows: 1, img: 'assets/img/aljazeera.jpg' },
-          { title: 'Twitter', cols: 1, rows: 1, img: 'assets/img/twitter-logo.png' },
-          { title: 'CNN', cols: 1, rows: 1, img: 'assets/img/cnn-logo.webp' },
-          { title: 'TVN24', cols: 1, rows: 1, img: 'assets/img/tvn24-logo.jpg' },
-          { title: 'Bloomberg', cols: 1, rows: 1, img: 'assets/img/bloomberg-tv-logo.webp' },
-          { title: 'RussiaToday', cols: 1, rows: 1, img: 'assets/img/russia-today.png' },
+          { title: 'BBC', cols: 1, rows: 1, img: 'assets/img/bbc-logo.png', mentions: 11 },
+          { title: 'Al Jazeera', cols: 1, rows: 1, img: 'assets/img/aljazeera.jpg', mentions: 7 },
+          { title: 'Twitter', cols: 1, rows: 1, img: 'assets/img/twitter-logo.png', mentions: 5 },
+          { title: 'CNN', cols: 1, rows: 1, img: 'assets/img/cnn-logo.png', mentions: 2 },
+          { title: 'TVN24', cols: 1, rows: 1, img: 'assets/img/tvn24-logo.jpg', mentions: 3 },
+          { title: 'Bloomberg', cols: 1, rows: 1, img: 'assets/img/bloomberg-tv-logo.png', mentions: 4 },
+          { title: 'RussiaToday', cols: 1, rows: 1, img: 'assets/img/russia-today.png', mentions: 0 },
         ];
       }
 
