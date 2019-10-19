ddc6ba8b4cf865fd45cb22f4d2d789103dcf8b99
kozaqui <kozaqui@gmail.com>
2019-10-19 15:03:12 +0200

    Plot actual data and do it in dark mode

--- strimini/src/app/transcript-to-plot-data.service.ts
+++ strimini/src/app/transcript-to-plot-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as transcriptData from './data';
+import * as transcriptData from '../assets/data.json';
 // import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -8,35 +8,13 @@ import * as transcriptData from './data';
 export class TranscriptToPlotDataService {
 
   public data: any = transcriptData;
-  
-  
 
-  // public data = JSON.parse(JSON.stringify(transcriptData));
   constructor() {
-    // this.http.get('assets/ordersummary.json')//, options)
-    // const configUrl = 'assets/data.json';
-
-    // this.http.get(configUrl).subscribe((data) => {
-    //   this.data = data;
-    //   console.log(this.data);
-
-    // });
-    // console.log(this.data);
-    console.error(this.data);
-  
-
+    // console.error(this.data);
    }
-  //  configUrl = 'assets/config.json';
 
-  //  getConfig() {
-  //    return this.http.get(this.configUrl);
-  //  }
-
-  // getConfig() {
-  //   return this.http.get(this.configUrl);
-  // }
   public generateFrequencyData() {
-    return this.data.dupa.map( (dataPoint) => {
+    return this.data.default.map( (dataPoint) => {
       return [dataPoint.offset, dataPoint.value];
     })
 
