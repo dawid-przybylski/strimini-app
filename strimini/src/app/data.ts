385801c4177415d72928991fb1bc0f42a0b16991
Dawid Przybylski <dawid.przybylski@symphony-solutions.eu>
2019-10-19 14:38:42 +0200

    update

---
+++ strimini/src/app/data.ts
@@ -0,0 +1,95 @@
+export var deafult = [
+    {
+        "text": "Elon Musk says Tesla",
+        "time": "2019-10-19 11:20:00",
+        "offset": 0,
+        "keywords": [
+            "tesla",
+            "musk"
+        ],
+        "value": 2
+    },
+    {
+        "text": "futuristic",
+        "time": "2019-10-19 11:20:02.158000",
+        "offset": 2158,
+        "keywords": [],
+        "value": 0
+    },
+    {
+        "text": "pickup truck will outperform a Porsche",
+        "time": "2019-10-19 11:20:03.265000",
+        "offset": 3265,
+        "keywords": [],
+        "value": 0
+    },
+    {
+        "text": "911",
+        "time": "2019-10-19 11:20:06.118000",
+        "offset": 6118,
+        "keywords": [],
+        "value": 0
+    },
+    {
+        "text": "okay that's what he say",
+        "time": "2019-10-19 11:20:07.090000",
+        "offset": 7090,
+        "keywords": [],
+        "value": 0
+    },
+    {
+        "text": "wedbush Tesla Island tides joins is now Dan welcome back to the show",
+        "time": "2019-10-19 11:20:08.423000",
+        "offset": 8423,
+        "keywords": [
+            "tesla"
+        ],
+        "value": 1
+    },
+    {
+        "text": "last time you were on",
+        "time": "2019-10-19 11:20:13.656000",
+        "offset": 13656,
+        "keywords": [],
+        "value": 0
+    },
+    {
+        "text": "you issued a code red",
+        "time": "2019-10-19 11:20:15.290000",
+        "offset": 15290,
+        "keywords": [],
+        "value": 0
+    },
+    {
+        "text": "Tesla stock",
+        "time": "2019-10-19 11:20:17.174000",
+        "offset": 17174,
+        "keywords": [
+            "tesla"
+        ],
+        "value": 1
+    },
+    {
+        "text": "I said it was going to go down a lot",
+        "time": "2019-10-19 11:20:18.647000",
+        "offset": 18647,
+        "keywords": [],
+        "value": 0
+    },
+    {
+        "text": "well it's come back up to nearly $200 a share what do you think about Tesla today",
+        "time": "2019-10-19 11:20:21.213000",
+        "offset": 21213,
+        "keywords": [
+            "tesla"
+        ],
+        "value": 1
+    },
+    {
+        "text": "look at me I think you still at the glass half empty",
+        "time": "2019-10-19 11:20:25.855000",
+        "offset": 25855,
+        "keywords": [],
+        "value": 0
+    }
+]
